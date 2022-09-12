d4f04d7d60dd9105bad4a9649b26770c1f856824
karmelp <101389533+karmelp@users.noreply.github.com>
2022-09-12 23:00:10 +0300

    design

---
+++ script.js
@@ -0,0 +1,19 @@
+/* Kasutaja saab lugeda kõiki ülesandeid - ülesannete pealkirja, kas on tehtud/tegemata. */
+
+
+/* Kasutaja näeb, mitu ülesannet on tehtud, mitu on tegemata. */
+
+
+/* Kasutaja saab lisada uue ülesande. */
+
+
+/* Kasutaja saab märkida ülesande tehtuks. */
+
+
+/* Kasutaja saab ülesande muuta ülesannet. */
+
+
+/* Kasutaja saab ülesande kustutada */
+
+
+/* Kasutaja saab kõik ülesanded korraga ära kustutada */
