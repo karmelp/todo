931128132bbde4d75cdd84990a32c2c6212a2bb5
karmelp <101389533+karmelp@users.noreply.github.com>
2022-09-13 17:14:42 +0300

    Array

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-/* Kasutaja saab lugeda kõiki ülesandeid - ülesannete pealkirja, kas on tehtud/tegemata. */
+//Kasutaja saab lugeda kõiki ülesandeid - ülesannete pealkirja, kas on tehtud/tegemata.
+        const tasks = ['Ülesanne1', 'Ülesanne 2', 'Ülesanne 3'];
+
+        function readTask() {
+            const taskList = document.querySelector('.taskList');
+            const li = document.createElement('li')
+            li.appendChild(document.createTextNode(tasks));;
+            li.setAttribute("class", "taskListItem");
+            taskList.appendChild(li);
+            console.log(taskList)
+        } 
+
+        tasks.forEach(task => {
+            readTask()
+        });
 
 
-/* Kasutaja näeb, mitu ülesannet on tehtud, mitu on tegemata. */
+// Kasutaja näeb, mitu ülesannet on tehtud, mitu on tegemata.
 
 
-/* Kasutaja saab lisada uue ülesande. */
+// Kasutaja saab lisada uue ülesande.
 
 
-/* Kasutaja saab märkida ülesande tehtuks. */
+// Kasutaja saab märkida ülesande tehtuks.
 
 
-/* Kasutaja saab ülesande muuta ülesannet. */
+// Kasutaja saab ülesande muuta ülesannet.
 
 
-/* Kasutaja saab ülesande kustutada */
+// Kasutaja saab ülesande kustutada.
 
 
-/* Kasutaja saab kõik ülesanded korraga ära kustutada */
+// Kasutaja saab kõik ülesanded korraga ära kustutada.
