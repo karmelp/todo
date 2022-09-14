56a155daefdee02dbdf5c1b8663ae465248d4daa
karmelp <101389533+karmelp@users.noreply.github.com>
2022-09-14 14:20:15 +0300

    essa

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
 //Kasutaja saab lugeda kõiki ülesandeid -
         // Ülesannete pealkirja
         const tasks = [
-            {title: 'Exercise 1', isDone: true},
-            {title: 'Exercise 2', isDone: true},
-            {title: 'Exercise 3', isDone: false},
+            {title: 'Exercise 1', isDone: 'done'},
+            {title: 'Exercise 2', isDone: 'done'},
+            {title: 'Exercise 3', isDone: 'not done'},
         ];
 
         function showTask(task) {
-            const taskList = document.querySelector('.taskList');
-            const li = document.createElement('li')
-            li.appendChild(document.createTextNode(task.title));;
-            li.setAttribute("class", "taskListItem");
-            taskList.appendChild(li);
+            // vöta HTMList tasklist klassiga element
+            const taskList = document.querySelector('.taskList')
+            
+            // loo list item staatuse jaoks
+            const taskStatusText = document.createElement("li");
+
+            // loo list item pealkirja jaoks
+            const taskTitleText = document.createElement("li");
+
+            // pane pealkirja list itemi sisse lapselement, mis on tekst "text.title"
+            taskTitleText.appendChild(document.createTextNode(task.title));
+
+            // pane staatuse list itemi sisse lapselement, mille tekst on kas "tehtud" vöi "tegemata" olenevalt staatusest
+            taskStatusText.appendChild(document.createTextNode(task.isDone));
+
+            // lisa pealkirja elemendile klass "taskListItem"
+            taskTitleText.setAttribute("class","taskListItem");
+
+            // lisa staatuse elemendile klass "taskListItem"
+            taskStatusText.setAttribute("class", "taskListItem")
+
+            // lisa tasklist klassiga elemendile pealkirja element
+            taskList.appendChild(taskTitleText);
+
+            // Lisa tasklist klassiga elemendile staatuse element
+            taskList.appendChild(taskStatusText)
         } 
 
         tasks.forEach(task => {
             showTask(task)
         });
 
-        // Kas on tehtud/tegemata.
-        
-        
 
 
 // Kasutaja näeb, mitu ülesannet on tehtud, mitu on tegemata.
